b1b52e79e29cb01d86b39dae5f67690bbda32b4b
Priscilla Gasztowtt <pgasztowtt@SIA-Conseil.local>
2021-11-03 21:44:04 +0100

    75%

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import contacts from './contacts.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const firstContacts = contacts.slice(0,5)
+  // transformer la function App en class avec state
+
+const addRandomContact = contacts[Math.floor(Math.random()*contacts.length)]
+
+// const newContactsList = firstContacts.push
+
+class App extends React.Component {
+  state= {
+    contacts: firstContacts,
+  };
+
+  function addRandomContact() => {
+    const stateCopy = [...this.state.contacts]
+    stateCopy.push(contacts[Math.floor(Math.random()*contacts.length)])
+
+    this.setState({
+      contacts:stateCopy
+    })
+  }
+
+  sortByName = () => {
+    const stateCopy = [...this.state.contacts]
+    stateCopy.sort((a,b) => a.name - b.name)
+    //a.name.localeCompare(b.name)
+
+    this.setState({
+      contacts:stateCopy
+    })
+  };
+
+  render() {
+    return (
+      <div className="App">
+          <button onClick={this.addRandom}> Add a random contact </button>
+          <button onClick={this.sortByName}> Sort contacts by name </button>
+      <h1>IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.contacts.map((contacts) => {
+            return ( 
+              <tr>
+                  <td><img src="{contacts.pictureUrl}" alt="default image"/></td>
+                  <td>{contacts.name}</td>
+                  <td>{contacts.popularity}</td>
+              </tr>          
+            )
+          })}
+        </tbody>
+      </table>
+      </div>
+    );                        
+  }
+  //il faut toujours donner une clef et une valeur à une state
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <table className="firstContacts">
+//         <firstContacts.pictureUrl />
+//         <firstContacts.name />
+//         <firstContacts.popularity />
+//       </table>
+//     </div>
+//   )
+// }
 
 export default App;
